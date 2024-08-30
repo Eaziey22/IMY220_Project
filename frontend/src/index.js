3d441f1ec82bc8d8cadba5b7630442c9eb9f2390
Eaziey22 <u21612812@tuks.co.za>
2024-08-30 10:39:46 +0200

    initial commit

---
+++ frontend/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+class Greetings extends React.Component{
+    render(){
+        return (
+            <div>
+                <h2>Hello Eaziey</h2>
+            </div>
+        )
+    }
+}
+
+const root = ReactDom.createRoot (document.getElementById("root"));
+root.render(<Greetings/>)
